6f45b3a3880254d91f7f8120223149a012fb200b
Adrià Sala <asala@redhat.com>
2025-11-03 13:37:53 +0100

    fix: prompt new vars

--- template/boilerplate_update.sh
+++ template/boilerplate_update.sh
@@ -29,14 +29,14 @@ echo "Running 'copier update'..."
 if git rev-parse --verify copier-boilerplate-sync >/dev/null 2>&1; then
     git switch --force-create copier-boilerplate-sync
 else
-    git checkout -b copier-boilerplate-sync --track origin/devel
+    git checkout -b copier-boilerplate-sync --track origin/copier
 fi
 # At this point stop the script if any of the following commands fail for any reason
 set -e
 if [[ $APPROVE == true ]]; then
-    copier recopy --defaults --skip-answered
+    copier update --skip-answered --defaults
 else
-    copier recopy
+    copier update --skip-answered
 fi
 # Check for merge conflicts
 if git status --porcelain | grep '^UU' > /dev/null || find . -name "*.rej" -print | grep '' > /dev/null; then
@@ -46,4 +46,4 @@ fi
 # commit and push
 git add .
 git commit -m "chore: copier-boilerplate sync for $(date +"%Y-%m-%d %H:%M:%S")"
-git push --force -o merge_request.create -o merge_request.target=devel -o merge_request.description="This PR was created by automated script" origin copier-boilerplate-sync
+git push --force -o merge_request.create -o merge_request.target=copier -o merge_request.description="This PR was created by automated script" origin copier-boilerplate-sync
